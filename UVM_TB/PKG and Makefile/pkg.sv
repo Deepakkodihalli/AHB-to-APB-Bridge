962ae10f76bb9f3e2dcae4fcfa421daaa6951e84
Deepak K <deepakkodihalli01@gmail.com>
2024-09-26 20:18:27 +0530

    Add files via upload

---
+++ UVM_TB/PKG and Makefile/pkg.sv
@@ -0,0 +1,29 @@
+package pkg;
+
+import uvm_pkg::*;
+
+`include "uvm_macros.svh"
+
+`include "ahb_config.sv"
+`include "ahb_trans.sv"
+`include "ahb_seqs.sv"
+`include "ahb_seqr.sv"
+`include "ahb_drv.sv"
+`include "ahb_mon.sv"
+`include "ahb_agt.sv"
+`include "ahb_agt_top.sv"
+
+`include "apb_config.sv"
+`include "apb_trans.sv"
+`include "apb_seqr.sv"
+`include "apb_driver.sv"
+`include "apb_mon.sv"
+`include "apb_agt.sv"
+`include "apb_agt_top.sv"
+
+`include "env_config.sv"
+`include "SB.sv"
+`include "env.sv"
+`include "test.sv"
+
+endpackage
